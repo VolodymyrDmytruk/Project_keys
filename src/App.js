56e633d23a07a44dfbd09bf9e14c22a73c165f99
VolodymyrDmytruk <dmytruk.vova@gmail.com>
2023-06-28 23:16:27 +0300

    implemented task1

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import React from 'react';
 import './App.css';
 
 export default function App() {
-  return (
-    <div>
-      Some List:
-    </div>
-  );
+	let someList = [
+		{ id: 1, text: 'Animals' },
+		{ id: 2, text: 'Anime' },
+		{ id: 3, text: 'Anti-Malware' },
+		{ id: 4, text: 'Art Design' },
+		{ id: 5, text: 'Books' },
+		{ id: 6, text: 'Business' },
+		{ id: 7, text: 'Calendar' },
+		{ id: 8, text: 'Cloud Storage' },
+		{ id: 9, text: 'File Sharing' },
+		{ id: 10, text: 'Animals' },
+		{ id: 11, text: 'Continuous Integration' },
+		{ id: 12, text: 'Cryptocurrency' }
+	];
+
+	return (
+		<div>
+			Some List:
+			<ul>
+				{someList.map(el => (
+					<li key={el.id}>{el.text}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
